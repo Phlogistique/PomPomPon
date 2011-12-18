63dc845fdd4cae814e8f0aadeb2c49919ad09fde
Noe Rubinstein <noe.rubinstein@gmail.com>
2011-12-18 13:19:12 +0100

    semi-working python version

--- pompom.py
+++ pompom.py
@@ -80,20 +80,25 @@ class PomPom:
         print "color at (%d,%d) = (%d,%d,%d)" % (x, y, hue, sat, val)
 
     def __init__(self):
+        self.init_params()
+        self.init_ui()
+        self.init_cv()
+        self.game()
 
-        # init parameters
-
+    def init_params(self):
         self.dilatation = 3
 
-        initial_aperture = 10
-        self.set_aperture(initial_aperture)
+        self.initial_aperture = 10
+        self.set_aperture(self.initial_aperture)
 
-        self.size = (320, 240) #FIXME
+        self.size = (320, 240)
 
         self.pompon = [PomPomPon(), PomPomPon()]
         self.pomponi = cycle(self.pompon)
 
-        # init UI
+        self.POSITIONS = map(self.coord, self.POSITIONS)
+
+    def init_ui(self):
         cv.NamedWindow(self.INPUT_WINDOW, cv.CV_WINDOW_AUTOSIZE)
         cv.NamedWindow(self.PROCESSING_WINDOW, cv.CV_WINDOW_AUTOSIZE)
 
@@ -106,10 +111,11 @@ class PomPom:
         cv.CreateTrackbar("Val tolerance", self.PROCESSING_WINDOW,
                 self.pompon[0].val_tolerance, 127, self.cb_set("val_tolerance"))
         cv.CreateTrackbar("Flou", self.PROCESSING_WINDOW,
-                initial_aperture, 20, self.set_aperture)
+                self.initial_aperture, 20, self.set_aperture)
         cv.CreateTrackbar("Dilatation", self.PROCESSING_WINDOW,
                 3, 20, self.cb_set("dilatation"))
 
+    def init_cv(self):
         # init image capture
         self.camera = cv.CaptureFromCAM(-1) # any camera
 
@@ -124,22 +130,29 @@ class PomPom:
         self.bin_frame      = img(1)
         self.mask           = img(1)
 
+
     def in_range(self, pompon, src, dst):
         cv.InRangeS(src, pompon.lower, pompon.upper, dst);
 
-    def target(center, color, radius = 30):
+    def target(self, center, color, radius = 30):
         for i in [3,2,1]:
-            cv.Circle(self.frame, (x, y), radius * i / 3.0, color, thickness=3)
+            cv.Circle(self.frame, center, int(radius * i / 3.0), color, thickness=3)
 
-    def coord(frac):
-        return frac
+    def coord(self, frac, y=None):
+        x, y = frac if y is None else (frac, y)
+        sx, sy = self.size
 
-    POSITIONS = map(coord, [
+        x = int(((x + 1) * sx) / 2)
+        y = int(((y + 1) * sy) / 2)
+
+        return (x,y)
+
+    POSITIONS = [
     #  (-1,1)             (1,1)
     #   +------------------+
     #   |                  |
     #   |                  |
-    #   |                  |
+    #   |                  |
     #   |                  |
     #   |                  |
     #   +------------------+
@@ -150,42 +163,53 @@ class PomPom:
             (     0,  3.0/4),
             ( 1.0/3,  1.0/2),
             ( 2.0/3,      0),
-            ( 1.0/3, -1.0/2)])
+            ( 1.0/3, -1.0/2)]
 
     RED = cv.CV_RGB(255,0,0)
     BLUE = cv.CV_RGB(0,0,255)
 
-    def wait(self, seconds):
+    def do_for(self, seconds, f):
         start = time.time()
         while time.time() - start < seconds:
-            self.step.next()
+            f()
+            self.loop.next()
+
+    def do_until(self, cond, f):
+        while not cond():
+            f()
+            self.loop.next()
+
+    def do_forever(self, f):
+        while True:
+            f()
+            self.loop.next()
+
         
     def game(self):
-        loop = game.loop()
+        self.loop = self.run()
 
-        self.left = self.coord((-0.7,0.7))
-        self.right = self.coord((0.7,0.7))
-        self.left_color = cv.CV_RGB(255,100,0))
-        self.right_color = cv.CV_RGB(0,100,255))
+        self.left = self.coord(-0.7,0.7)
+        self.right = self.coord(0.7,0.7)
+        self.left_color = cv.CV_RGB(255,100,0)
+        self.right_color = cv.CV_RGB(0,100,255)
 
-        self.step = self.calibration_step
-        self.wait(1)
+        next(self.loop) # retrieve first frame
+        self.do_for(1, self.draw_calibration_targets)
+        self.calibration_done = False
+        self.do_until(lambda: self.calibration_done, self.calibration_step)
+        self.do_forever(self.play_step)
 
-        while not self.calibration_done:
-            self.step.next()
-
-        self.step = self.play_step
-        while True:
-            self.step.next()
-
-    def calibration_step(self):
+    def draw_calibration_targets(self):
         self.target(self.left, self.left_color)
         self.target(self.right, self.right_color)
+
+    def calibration_step(self):
+        self.draw_calibration_targets()
 
     def play_step(self):
         pass
 
-    def loop(self):
+    def run(self):
 
         def next_frame():
             self.frame = cv.QueryFrame(self.camera)
@@ -233,7 +257,7 @@ class PomPom:
 
         # No webcam frame?
         print "No webcam frame! Exiting..."
-        sys.exit 1
+        sys.exit(1)
 
-a = PomPom().run()
+a = PomPom()
 
